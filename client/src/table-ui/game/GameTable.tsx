03a9f8fbbc11369cd4436e63a43e5128854aeacb
Tom Garner <tombuxdao@Toms-iMac.Home>
2025-09-06 03:22:47 +0100

    Add comprehensive socket event debugging to understand why game_update events are not being received

--- client/src/table-ui/game/GameTable.tsx
+++ client/src/table-ui/game/GameTable.tsx
@@ -502,7 +502,9 @@ export default function GameTable({
     
     socket.on('countdown_start', handleCountdownStart);
     socket.on("game_update", (updatedGame) => {
-      console.log("SOCKET EVENT: game_update", updatedGame);
+    socket.onAny((eventName, ...args) => {
+      console.log("🔍 [SOCKET DEBUG] Received event:", eventName, args);
+    });    console.log("🔍 [DEBUG] Setting up game_update event listener");      console.log("SOCKET EVENT: game_update", updatedGame);
       setGameState(updatedGame);
     });    socket.on('bidding_ready', handlePlayerActed);
     socket.on('bidding_complete', handlePlayerActed);
