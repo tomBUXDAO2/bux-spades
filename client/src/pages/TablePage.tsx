7018c1086df5207ef7d306aeeb4867e87cecedb5
Tom Garner <tombuxdao@Toms-iMac.Home>
2025-09-06 03:52:47 +0100

    Fix syntax error in TablePage.tsx - remove malformed console.log

--- client/src/pages/TablePage.tsx
+++ client/src/pages/TablePage.tsx
@@ -712,8 +712,7 @@ export default function TablePage() {
         )}
         
         <GameTable
-        console.log("🚨🚨🚨 [CRITICAL DEBUG] About to render GameTable with game:", game);          game={game}
-          onLeaveTable={handleLeaveTable}
+        console.log("🚨🚨🚨 [CRITICAL DEBUG] About to render GameTable with game:", game);          onLeaveTable={handleLeaveTable}
           startGame={handleStartGame}
           user={user}
           showStartWarning={showStartWarning}
